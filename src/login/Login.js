6ca20af23268d2adbd6792024366370334441480
danielmilewski <Daniel.Milewski86@wp.pl>
2016-10-30 13:21:26 +0100

    Poprawki w logowaniu vol.2

--- src/login/Login.js
+++ src/login/Login.js
@@ -7,21 +7,22 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-    loginSuccess: (userData) => dispatch ({type: 'LOGIN_SUCCESS', data: userData.profileObj}),
-    loginFailure: () => dispatch ({ type: 'LOGIN_FAILURE'}),
-    logOut: () => dispatch ({ type: 'LOG_OUT'})
+    loginSuccess: (userData) => dispatch({type: 'LOGIN_SUCCESS', data: userData.profileObj}),
+    loginFailure: () => dispatch({type: 'LOGIN_FAILURE'}),
+    logOut: () => dispatch({type: 'LOG_OUT'})
 })
 
 const Login = (props) => (
     <div>
         <h1>Login</h1>
-        <button onClick={() => props.logOut()}> log out </button>:
+        <button onClick={() => props.logOut()}> Log out</button>
+        
         <GoogleLogin
             clientId="630341204564-at7usoc2ueh94d7q9bucfrlktv3nvqpc.apps.googleusercontent.com"
-            buttonText ="Log in"
+            buttonText="Log in"
             onSuccess={props.loginSuccess}
             onFailure={props.loginFailure}
-            />
+        />
     </div>
 )
 
