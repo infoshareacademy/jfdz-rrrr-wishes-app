911e6c5d3d578eb10cd9b98ba8bc167783abbb45
Daniel Milewski <Daniel.Milewski86@wp.pl>
2016-10-31 09:47:35 +0100

    dodanie class do button

--- src/login/Login.js
+++ src/login/Login.js
@@ -15,9 +15,9 @@ const mapDispatchToProps = (dispatch) => ({
 const Login = (props) => (
     <div>
         <h1>Login</h1>
-        <button onClick={() => props.logOut()}> Log out</button>
+        <button className="logOut" onClick={() => props.logOut()}> Log out</button>
         
-        <GoogleLogin
+        <GoogleLogin className="logIn"
             clientId="630341204564-at7usoc2ueh94d7q9bucfrlktv3nvqpc.apps.googleusercontent.com"
             buttonText="Log in"
             onSuccess={props.loginSuccess}
