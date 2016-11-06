5bc541349149db1b4c9602851ec9226f3e48232a
danielmilewski <Daniel.Milewski86@wp.pl>
2016-11-06 14:01:30 +0100

    Poprawki z Calendar

--- src/calendar/Calendar.js
+++ src/calendar/Calendar.js
@@ -2,8 +2,10 @@ import React from 'react'
 import {connect} from 'react-redux'
 import BigCalendar from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
-import events from './events'
+
 import moment from 'moment'
+
+
 
 BigCalendar.setLocalizer(
     BigCalendar.momentLocalizer(moment)
@@ -13,21 +15,71 @@ const mapStateToProps = (state) => ({
     calendar:state.calendar
 })
 
-const Calendar = (props) =>(
-    <div>
-        <BigCalendar
-            selectable
-            events={events}
-            timeslots={3}
-            defaultView='week'
-            scrollToTime={new Date(1970, 1, 1, 6)}
-            defaultDate={new Date(2015, 3, 12)}
-            onSelectEvent={event => alert(event.title)}
-            onSelectSlot={(slotInfo) => alert(
-            `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
-            `\nend: ${slotInfo.end.toLocaleString()}`
-        )}
-        />
-    </div>
-)
+
+class Calendar extends React.Component {
+    render() {
+        return <div id="calendar"></div>;
+    }
+    componentDidMount() {
+        $('#calendar').fullCalendar({
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay'
+            },
+            editable: true,
+            droppable: true, // this allows things to be dropped onto the calendar
+            drop: function() {
+                // is the "remove after drop" checkbox checked?
+                if ($('#drop-remove').is(':checked')) {
+                    // if so, remove the element from the "Draggable Events" list
+                    $(this).remove();
+                }
+            }
+        })
+    }
+}
+
+class External extends React.Component {
+    render() {
+        return <div id='external-events'>
+            <h4>Draggable Events</h4>
+            <div className='fc-event'>My Event 1</div>
+            <div className='fc-event'>My Event 2</div>
+            <div className='fc-event'>My Event 3</div>
+            <div className='fc-event'>My Event 4</div>
+            <div className='fc-event'>My Event 5</div>
+            <p>
+                <input type='checkbox' id='drop-remove' />
+                <label for='drop-remove'>remove after drop</label>
+            </p>
+        </div>;
+    }
+    componentDidMount() {
+        $('#external-events .fc-event').each(function() {
+
+            // store data so the calendar knows to render an event upon drop
+            $(this).data('event', {
+                title: $.trim($(this).text()), // use the element's text as the event title
+                stick: true // maintain when user navigates (see docs on the renderEvent method)
+            });
+
+            // make the event draggable using jQuery UI
+            $(this).draggable({
+                zIndex: 999,
+                revert: true,      // will cause the event to go back to its
+                revertDuration: 0  //  original position after the drag
+            });
+        });
+    }
+}
+
+
+/*
+ * Render the above component into the div#app
+ */
+React.render(<Application />, document.getElementById('app'));
+
 export default connect(mapStateToProps)(Calendar)
+
+
